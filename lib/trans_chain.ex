f444fbd5a413d03894e7a8b11c0cbf0e7f44d39c
TraceyOnim <traceyonim22@gmail.com>
2022-08-20 02:12:52 +0300

    get transaction info

--- lib/trans_chain.ex
+++ lib/trans_chain.ex
@@ -1,12 +1,22 @@
 defprotocol TransChain do
-  def send_transaction(params)
+  def transaction(params)
 end
 
 alias TransChain.Ethereum
 
 defimpl TransChain, for: Ethereum do
-  def send_transaction(params), do: Ethereum.send_transaction(params)
+  def transaction(%Ethereum{method: "eth_sendTransaction"} = params) do
+    Ethereum.send_transaction(params)
+  end
+
+  def transaction(%Ethereum{method: "eth_getTransactionByHash"} = params) do
+    Ethereum.get_transaction(params)
+  end
 end
+
+# defimpl TransChain, for: Ethereum do
+#   def transaction(params), do: Ethereum.get_transaction(params)
+# end
 
 # defimpl TransChain, for: Bitcoin do
 #   def send_transaction(params), do: Bitcoin.send_transaction()
--- lib/trans_chain/Ethereum.ex
+++ lib/trans_chain/Ethereum.ex
@@ -5,20 +5,40 @@ defmodule TransChain.Ethereum do
   require Logger
   alias __MODULE__
 
-  @type t :: %Ethereum{params: [map()]}
+  @type t :: %Ethereum{params: [any()], method: String.t()}
 
-  defstruct ~w(params)a
+  defstruct ~w(params method)a
 
-  def send_transaction(%__MODULE__{params: params}) do
+  def send_transaction(%__MODULE__{params: params, method: method}) do
     url()
-    |> HTTPoison.post!(body(params), headers())
+    |> HTTPoison.post!(body(params, method), headers())
     |> response()
+  end
+
+  def get_transaction(%__MODULE__{params: params, method: method}) do
+    url()
+    |> HTTPoison.post!(body(params, method), headers())
+    |> transaction_info()
+  end
+
+  defp transaction_info(%HTTPoison.Response{body: body, status_code: 200}) do
+    case Poison.decode!(body) do
+      %{"result" => nil} ->
+        IO.puts("No matching transaction")
+
+      %{"result" => result} ->
+        Enum.each(result, fn {k, v} -> IO.puts("#{k}:  #{v}") end)
+    end
+  end
+
+  defp transaction_info(_) do
+    Logger.error("Failed to fetch transaction info, Try Again!!")
   end
 
   defp response(%HTTPoison.Response{body: body, status_code: 200}) do
     case Poison.decode!(body) do
       %{"result" => %{"tx" => tx}} ->
-        Logger.info("Transaction status" <> ":" <> "complete successfully")
+        Logger.info("complete successfully")
         Logger.info("Transaction hash" <> ":" <> "#{tx["hash"]}")
         Logger.info("Transaction gas" <> ":" <> "#{tx["gas"]}")
 
@@ -31,11 +51,11 @@ defmodule TransChain.Ethereum do
     Logger.error("Failed to issue transaction, Try Again!!")
   end
 
-  defp body(params) do
+  defp body(params, method) do
     %{
       id: 1,
       jsonrpc: "2.0",
-      method: "eth_sendTransaction",
+      method: method,
       params: params
     }
     |> Poison.encode!()
